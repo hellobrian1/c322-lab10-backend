acf8fa3c91e52276e336af2001e052066a087065
hellobrian1 <143294125+hellobrian1@users.noreply.github.com>
2024-04-22 20:50:16 -0400

    committing

--- src/main/java/edu/iu/habahram/ducksservice/model/Customer.java
+++ src/main/java/edu/iu/habahram/ducksservice/model/Customer.java
@@ -1,7 +1,67 @@
 package edu.iu.habahram.ducksservice.model;
 
-public record Customer(String username,
-                       String password,
-                       String email) {
+import java.util.Objects;
+
+public class Customer {
+    private String username;
+    private String password;
+    private String email;
+
+    public Customer(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // If needed, you can also provide setter methods
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(getUsername(), customer.getUsername()) &&
+                Objects.equals(getPassword(), customer.getPassword()) &&
+                Objects.equals(getEmail(), customer.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getPassword(), getEmail());
+    }
+
 
 }
+
